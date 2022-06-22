a4843a095752729ec6358f17cacc07f48ada4a5b
dew01ke <me@andrey-volkov.ru>
2022-06-23 01:27:42 +0300

    Add scoring module

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,9 +1,7 @@
 import { GreetingsHandler } from '@/handlers/greetings';
-import { UserInfoHandler } from '@/handlers/userInfo';
 import { ScoringHandler } from '@/handlers/scoring';
 
 export default [
   GreetingsHandler,
-  UserInfoHandler,
   ScoringHandler,
 ]
